0e4b79283d8e64d8aaaa7f82781682f4e6df8e2b
kairamumasatyasubhash <skairam@softility.com>
2025-11-05 13:00:49 +0530

    Update caluclator.groovy

--- src/com/subhash/builds/caluclator.groovy
+++ src/com/subhash/builds/caluclator.groovy
@@ -1,15 +1,14 @@
-//packages installing
-package com.subhash.builds;
-//defining the class
-class caluclator {
-    //define a varibles and defining the varibles
-    def jenkins
-    caluclator(jenkins) {
-        this.jenkins = jenkins
-}
-//creation of a method in addition method
-def add(firsnumber, secoundnumber) {
-    //logic of add numbers
-    return firsnumber + secoundnumber
-}
+package com.sujith.builds
+
+class Calculator{
+    def jenkins 
+
+    Calculator(jenkins){
+      this.jenkins = jenkins 
+    }
+
+    def add(firstNumber, secNumber)
+    {
+        return firstNumber + secNumber;
+    }
 }
